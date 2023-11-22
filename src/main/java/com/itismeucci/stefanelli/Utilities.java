a4a6c6f0bd37de949d01da76189f6df8c04a05f7
lai2-24 <lai2-24@itismeucci.com>
2023-11-22 08:48:20 +0100

    colors

--- src/main/java/com/itismeucci/stefanelli/Utilities.java
+++ src/main/java/com/itismeucci/stefanelli/Utilities.java
@@ -18,4 +18,13 @@ public class Utilities {
     public static final String purple = "\u001B[35m";
     public static final String cyan = "\u001B[36m";
     public static final String white = "\u001B[37m";
+
+    public static final String BLACK_BRIGHT = "\033[0;90m";  // BLACK
+    public static final String RED_BRIGHT = "\033[0;91m";    // RED
+    public static final String GREEN_BRIGHT = "\033[0;92m";  // GREEN
+    public static final String YELLOW_BRIGHT = "\033[0;93m"; // YELLOW
+    public static final String BLUE_BRIGHT = "\033[0;94m";   // BLUE
+    public static final String PURPLE_BRIGHT = "\033[0;95m"; // PURPLE
+    public static final String CYAN_BRIGHT = "\033[0;96m";   // CYAN
+    public static final String WHITE_BRIGHT = "\033[0;97m";  // WHITE
 }
